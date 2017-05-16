0c05499c818c0b2a77ad9bf1e362e2af913805cf
GrantManthey <grant.manthey8580@gmail.com>
2017-05-16 12:21:31 -0700

    Update quadratic.c

--- quadratic.c
+++ quadratic.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main(){
-float a,b,c,d,e,f,g,h,i,j,k;
+float a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
 
 
 printf("please enter an imput for a : \n");
@@ -14,7 +15,7 @@ scanf("%f",&b);
 printf("please enter an imput for c : \n");
 scanf("%f",&c);
 
-d = b*-1;
+d = -b;
 e = b*b;
 f = 4*(a*c);
 g = e - f;
@@ -22,18 +23,34 @@ h = sqrt(g);
 i = h/(2*a);
 j = d + i ;
 k = d - i;
-  if (g < 0) 
-{
-}
+l = abs(g);
+m = sqrt(l);
+n = -m/(2*a);
+o = d + n;
+p = d - n;
 
+ if (g < 0) {
+    printf(" the first answer is %f'imaginary'\n",o);
+    printf("the second answer is %f'imaginary'\n",p);
+}  else {
+   printf(" the first answer 'real' is : %f 'real'\n",j);
+    printf("the second answer 'real' is : %f 'real'\n",k);
+}
+printf("a is %f: \n",a);
+printf("b is %f: \n",b);
+printf("c is %f: \n",c);
 printf("d is %f: \n",d);
 printf("e is %f: \n",e);
 printf("f is %f: \n",f);
 printf("g is %f: \n",g);
 printf("h is %f: \n",h);
 printf("i is %f: \n",i);
-printf("answer 1 is %f: \n",j);
-printf("answer 2 is %f: \n",k);
-
+printf("j is %f: \n",j);
+printf("k is %f: \n",k);
+printf("l is %f: \n",l);
+printf("m is %f: \n",m);
+printf("n is %f: \n",n);
+printf("o is %f: \n",o);
+printf("p is %f: \n",p);
 return 0;
 }
