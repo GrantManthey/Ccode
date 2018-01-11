4fe2a955ae0bd692ad5fc44bb5edce76da64624d
GrantManthey <grant.manthey8580@gmail.com>
2018-01-11 09:12:06 -0800

    Create hellotime.c

---
+++ hellotime.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+int main(){
+  time_t t = time(NULL);
+  
+  printf("now: %d-%d-%d %d:%d:%d\n",
+  tm.tm year + 1900, tm.tm min, tm.tm sec);
+  tm.tm mday, tm.tm hour, tm.tm min, tm.tm sec);
+  }
